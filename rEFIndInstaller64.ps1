2de344239553a29c1006071b3eb903737e131f7f
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-08-02 09:25:50 -0400

    Add files via upload

---
+++ rEFIndInstaller64.ps1
@@ -0,0 +1,20 @@
+# Link de Descarga Directa, se puede Cambiar.
+$downloadUrl = "https://master.dl.sourceforge.net/project/linuxoneclick/rEFInd.zip?viasf=1"
+$outputFile = "C:\archivo.zip"
+$destinationFolder = "C:\"
+
+Write-Output "Descargando Archivo. Esto Tomara Tiempo, Relajate :)"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $outputFile
+
+Write-Output "Descomprimiendo. Esto tomara aun mas Tiempo, Relajate x2 :)"
+Expand-Archive -Path "$outputFile" -DestinationPath "$destinationFolder"
+
+Remove-Item $outputFile
+
+Write-Output "Descarga Completada"
+
+Write-Output "Instalando..."
+# Ubicacion del Script que se ejecuta al descomprimit.
+Start-Process "C:\rEFIndAjustes\Instalar.bat" -NoNewWindow -Wait
+
+Stop-Process -Id $PID
